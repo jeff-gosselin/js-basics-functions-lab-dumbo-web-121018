cd6ce7083b91e4cf2a586563fdf9e91f4904d684
Jeffrey T Gosselin <jgoz.email@gmail.com>
2019-01-27 16:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(street) {
   let location = 42;
-  if (strret > location) {
+  if (street > location) {
     let distance = street - location
   } else {
     let distance = location - street
   }
+  return distance;
 }
